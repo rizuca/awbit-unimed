ef3dc3480a30909b997f4e97a9c39ded9c1ab303
Rizuca <qiqiakun1@gmail.com>
2025-06-18 16:59:28 +0700

    Enable registration routes

--- routes/auth.php
+++ routes/auth.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -33,6 +34,12 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
+        Route::get('register', [RegisteredUserController::class, 'create'])
+->middleware('guest')
+->name('register');
+
+Route::post('register', [RegisteredUserController::class, 'store'])
+->middleware('guest');
 });
 
 Route::middleware('auth')->group(function () {
@@ -56,4 +63,11 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
-});
+        Route::get('register', [RegisteredUserController::class, 'create'])
+->middleware('guest')
+->name('register');
+
+Route::post('register', [RegisteredUserController::class, 'store'])
+->middleware('guest');
+}
+);
